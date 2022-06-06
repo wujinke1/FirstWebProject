a10600ad09d4408604419c4869483317f4880d67
wujinke1 <1376638160@qq.com>
2022-06-06 17:37:08 +0800

    commit TestDemo4

---
+++ src/com/javaTest/TestDemo4.java
@@ -0,0 +1,10 @@
+package com.javaTest;
+
+public class TestDemo4 {
+    public static void main(String[] args) {
+        System.out.println("TestDemo4 Git");
+        System.out.println("TestDemo4 Git");
+        System.out.println("TestDemo4 Git");
+        System.out.println("TestDemo4 Git");
+    }
+}
